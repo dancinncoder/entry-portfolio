eb7651fa1c2e8efbd6ba47eec27ae8601e77a8d5
Hamin Lee <whereanna7@gmail.com>
2023-12-17 18:09:36 +0900

    [ADD] story page css and wireframe

--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -4,7 +4,23 @@ import { styled } from "styled-components";
 function Story() {
   return (
     <StOuterFrame>
-      <div>Story</div>
+      <StStoryContainer>
+        <StStoryMiddleContainer>
+          <StTitle>3 months of learning</StTitle>
+          <StContentContainer>
+            <StLeftContent>StLeftContent</StLeftContent>
+            <StTabBarContainer>
+              <StTabBar>
+                <StCategoryCircle></StCategoryCircle>
+                <StCategoryCircle></StCategoryCircle>
+                <StCategoryCircle></StCategoryCircle>
+                <StCategoryCircle></StCategoryCircle>
+              </StTabBar>
+              <StButton>next</StButton>
+            </StTabBarContainer>
+          </StContentContainer>
+        </StStoryMiddleContainer>
+      </StStoryContainer>
     </StOuterFrame>
   );
 }
@@ -12,7 +28,6 @@ function Story() {
 export default Story;
 
 const StOuterFrame = styled.div`
-  border: 1px solid red;
   width: 100vw;
   height: 100vh;
   display: flex;
@@ -20,4 +35,83 @@ const StOuterFrame = styled.div`
   align-items: center;
   justify-content: center;
   overflow: hidden;
+  background-color: black;
+`;
+
+const StStoryContainer = styled.div`
+  border: 1px solid #353535;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 5rem;
+  width: 70rem;
+  height: 40rem;
+  color: white;
+`;
+
+const StTitle = styled.h1`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 2.5rem;
+  font-weight: 900;
+  text-align: left;
+`;
+
+const StStoryMiddleContainer = styled.div`
+  width: 55rem;
+  height: 35rem;
+  padding: 3rem;
+  border: 1px solid yellow;
+  display: flex;
+  gap: 1.5rem;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const StContentContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 30rem;
+  width: 45rem;
+  border: 1px solid red;
+  gap: 1rem;
+`;
+const StLeftContent = styled.div`
+  border: 1px solid blue;
+  width: 80%;
+  height: 90%;
+`;
+const StTabBarContainer = styled.div`
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 5rem;
+  width: 13%;
+  height: 90%;
+`;
+const StTabBar = styled.div`
+  border: 1px solid yellow;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+`;
+const StButton = styled.button``;
+const StCategoryCircle = styled.button`
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  background-color: #e9e9e9;
 `;
